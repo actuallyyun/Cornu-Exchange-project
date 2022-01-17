ee740767e3e697b0489094dca750cc0eb50bb5aa
Yun <irene.jiyun@gmail.com>
2022-01-17 11:41:20 +0100

    configured settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'commerce.settings.dev_settings')
+                          'commerce.settings.base_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
